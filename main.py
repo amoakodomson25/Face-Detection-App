7e0283177a54a9664f09a43757c55b41d3415740
Amoako Domson Wahab <143537125+amoakodomson25@users.noreply.github.com>
2025-07-06 12:18:50 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import cv2
+face_cascade = cv2.CascadeClassifier('trained_model/haarcascade_frontalface_default.xml')
+
+cam = cv2.VideoCapture(0)
+
+if not cam.isOpened():
+    print("Error: Cannot access webcam")
+    exit()
+
+while True:
+    # Read frame-by-frame
+    ret, frame = cam.read()
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5)
+
+    # Draw rectangles around detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Show result
+    cv2.imshow('Face Detection', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
